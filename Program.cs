474077ba99f952ceae41c1d67b90a641a306fa94
gourumanisha <manishareddygouru@gmail.com>
2023-10-05 15:38:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+    
+            {
+                int firstNumber, secondNumber;
+
+                Console.Write("Input the First Number: ");
+                firstNumber = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Input the Second Number: ");
+                secondNumber = Convert.ToInt32(Console.ReadLine());
+
+                // Swap the numbers
+                (firstNumber, secondNumber) = (secondNumber, firstNumber);
+
+                Console.WriteLine("After Swapping:");
+                Console.WriteLine("First Number: " + firstNumber);
+                Console.WriteLine("Second Number: " + secondNumber);
+                Console.ReadLine();
+            }
+        }
+
+
+    }
+}
+
+
